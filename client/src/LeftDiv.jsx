90598897e58f3f521a6eeb6307a95d1f655b5409
dhananjay8883 <136006593+dhananjay8883@users.noreply.github.com>
2024-09-14 15:59:44 +0530

    left_page layout done
    apply css as refering some of best agriculture websites

--- client/src/LeftDiv.jsx
+++ client/src/LeftDiv.jsx
@@ -1,8 +1,20 @@
+import React from "react";
+import "./LeftDiv.css";
 function LeftDiv() {
     return (
       <div className="LeftDiv">
-        <p>Left Div</p>
+      <div className="racat">
+      <button className="upload_button_left">upload +</button>
       </div>
+      <div className="line_leftside"></div>
+        <div className="group_button_left">
+         <div>User Input</div>
+        <button className="button_leftside">soil PH</button>
+        <button  className="button_leftside">S(Sulfur)</button>
+        <button  className="button_leftside">N(Nitrogen)</button>
+        <button  className="button_leftside">K(potassium)</button>
+      </div>
+    </div>
     )
   }
   
