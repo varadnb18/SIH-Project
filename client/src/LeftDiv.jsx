c22da6c661d8ac3d4268e6f4c979be7e77b6a116
dhananjay8883 <136006593+dhananjay8883@users.noreply.github.com>
2024-09-14 17:11:22 +0530

    added some css change if you want
    secondpageleft part done

--- client/src/LeftDiv.jsx
+++ client/src/LeftDiv.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import "./LeftDiv.css";
+
 function LeftDiv() {
     return (
-      <div className="LeftDiv">
-      <div className="racat">
-      <button className="upload_button_left">Upload +</button>
-      </div>
-      <div className="line_leftside"></div>
-        <div className="group_button_left">
-         <div>User Input</div>
-        <button className="button_leftside">soil PH</button>
-        <button  className="button_leftside">S(Sulfur)</button>
-        <button  className="button_leftside">N(Nitrogen)</button>
-        <button  className="button_leftside">K(potassium)</button>
-      </div>
-    </div>
-    )
-  }
-  
-  export default LeftDiv;
-  
+        <div className="LeftDiv">
+            <div className="racat">
+                <button className="upload_button_left">Upload+</button>
+            </div>
+            <div className="line_leftside"></div>
+            <div className="group_button_left">
+                <div className="user_info_left">User Input</div>
+                <input type="text" className="button_leftside" placeholder="Insert PH of Soil" />
+                <input type="text" className="button_leftside" placeholder="Insert Sulfer Presence" />
+                <input type="text" className="button_leftside" placeholder="Insert Nitrogen Presence"/>
+                <input type="text" className="button_leftside" placeholder="Insert Potassium Presence"/>
+            </div>
+        </div>
+    );
+}
+
+export default LeftDiv;
