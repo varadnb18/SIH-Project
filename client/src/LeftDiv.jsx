05425ab58efcb2687411bee1867a7d1329050304
dhananjay8883 <136006593+dhananjay8883@users.noreply.github.com>
2024-09-16 16:22:31 +0530

    hide scroll bar
    scroll bar hide

--- client/src/LeftDiv.jsx
+++ client/src/LeftDiv.jsx
@@ -5,7 +5,7 @@ function LeftDiv() {
     return (
         <div className="LeftDiv">
             <div className="racat">
-                <button className="upload_button_left">Upload+</button>
+                <button className="upload_button_left">Upload image</button>
             </div>
             <div className="line_leftside"></div>
             <div className="group_button_left">
@@ -15,8 +15,13 @@ function LeftDiv() {
                 <input type="text" className="button_leftside" placeholder="Insert Nitrogen Presence"/>
                 <input type="text" className="button_leftside" placeholder="Insert Potassium Presence"/>
             </div>
+            <div>
+                <button class="btn">
+                    Send
+                </button>
+            </div>
         </div>
     );
 }
 
-export default LeftDiv;
+export default LeftDiv;
